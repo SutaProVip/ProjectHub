b39dbca9d807c69c245c7b1c7ca83c35ad1d67a9
isami <123729096+SutaProVip@users.noreply.github.com>
2024-09-15 06:22:32 +0700

    Update spamroll.lua

--- sucvat/spamroll.lua
+++ sucvat/spamroll.lua
@@ -1,4 +1,43 @@
 _G.isRolling = false
+function sellunit()
+  if _G.autosellRare == true then
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Auto-sell Status",
+    Text = "Auto-sell Rare is enabled",
+    Duration = 5,
+    })
+  wait(0.1)
+    local args = {[1] = "Toggle", [2] = "Rare"}
+  game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("Settings"):WaitForChild("SettingsEvent"):FireServer(unpack(args))
+  end
+  if _G.autosellEpic == true then
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Auto-sell Status",
+    Text = "Auto-sell Epic is enabled",
+    Duration = 5,
+    })
+  wait(0.1)
+    local args = {[1] = "Toggle", [2] = "Epic"}
+  game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("Settings"):WaitForChild("SettingsEvent"):FireServer(unpack(args))
+  end
+  if _G.autosellLegend == true then
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Auto-sell Status",
+    Text = "Auto-sell Legend is enabled",
+    Duration = 5,
+    })
+  wait(0.1)
+    local args = {[1] = "Toggle", [2] = "Legendary"}
+  game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("Settings"):WaitForChild("SettingsEvent"):FireServer(unpack(args))
+  end
+  if _G.autosellRare == false and _G.autosellRare == false and _G.autosellLegend == false then
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Auto-sell Status",
+    Text = "Auto-sell is disable",
+    Duration = 5,
+    })
+end
+  sellunit()
 function roll()
   while _G.isRolling do
     if _G.autorollx10 == true then
@@ -15,9 +54,19 @@ end
 function toggleRoll()
   _G.isRolling = not _G.isRolling
   if _G.isRolling then
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Auto-roll Status",
+    Text = "Auto-roll is enabled",
+    Duration = 5,
+    })
     print("Auto-roll đã bật")
     roll()
   else
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Auto-roll Status",
+    Text = "Auto-roll is disable",
+    Duration = 5,
+    })
     print("Auto-roll đã tắt")
   end
 end
