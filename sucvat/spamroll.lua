5651cd436fab07041e395bbfd87a684601c69eb3
isami <123729096+SutaProVip@users.noreply.github.com>
2024-09-15 07:13:57 +0700

    Update spamroll.lua

--- sucvat/spamroll.lua
+++ sucvat/spamroll.lua
@@ -1,4 +1,73 @@
 _G.isRolling = false
+function redeemcode()
+  local args = {[1] = "Interact", [2] = {[1] = "StarterUnitDialogue", [2] = 1, [3] = "Okay!"}}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("State"):WaitForChild("DialogueEvent"):FireServer(unpack(args))
+wait(0.1)
+local args = {[1] = "UpdateStatus", [2] = 2}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("MatchStatusEvent"):FireServer(unpack(args))
+wait(0.0001)
+local args = {[1] = "Interact",[2] = {[1] = "StarterUnitDialogue",[2] = 2,[3] = "Yeah!"}}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("State"):WaitForChild("DialogueEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "Select",[2] = "Luffo"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("Units"):WaitForChild("UnitSelectionEvent"):FireServer(unpack(args))
+wait(0.0001)
+  local args = {[1] = "Toggle", [2] = "SkipSummonAnimation"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("Settings"):WaitForChild("SettingsEvent"):FireServer(unpack(args))
+wait(0.0001)
+local args = {[1] = "10MVISITS"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "25MVISITS"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "400KLIKES"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "300KLIKES"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "200KLIKES"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "100KLIKES"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "10KLIKES"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "RELEASE"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "DELAY"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "300KPLAYERS"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "100MVISITS"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+local args = {[1] = "800KLIKES"}
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("CodesEvent"):FireServer(unpack(args))
+wait(0.00001)
+end
+function toggleCode()
+  if _G.redeemAllCode == true then
+    redeemcode()
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Redeem-Code Status",
+    Text = "Redeem-Code is enabled",
+    Duration = 5,
+    })
+  else
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Redeem-Code Status",
+    Text = "Redeem-Code is disable",
+    Duration = 5,
+    })
+  end
+end
 function sellunit()
   if _G.autosellRare == true then
     game.StarterGui:SetCore("SendNotification", {
@@ -77,7 +146,7 @@ local isToggled = false -- Biến để theo dõi trạng thái của nút
 screenGui.Parent = game.CoreGui
 toggleButton.Parent = screenGui
 toggleButton.Text = "Auto-roll"
-toggleButton.Size = UDim2.new(0, 50, 0, 30)
+toggleButton.Size = UDim2.new(0, 200, 0, 50)
 toggleButton.Position = UDim2.new(0.5, -100, 0.9, -25)
 -- Màu ban đầu (trạng thái tắt)
 toggleButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0) -- Đỏ
@@ -91,6 +160,6 @@ else
   -- Nếu trạng thái tắt
   toggleButton.Text = "Auto-roll Off"
   toggleButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0) -- Đỏ
- end
-    toggleRoll()
+end
+toggleRoll()
 end)
